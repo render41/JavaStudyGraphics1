ce9cbbc2255baee4d992ec90b0d979e2a08ea6dd
Gabriel Miranda <48923787+render41@users.noreply.github.com>
2021-01-03 20:10:13 -0300

    Add files via upload

--- Graphics2/src/Graphics/Game.java
+++ Graphics2/src/Graphics/Game.java
@@ -3,7 +3,9 @@ package Graphics;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
@@ -14,16 +16,35 @@ public class Game extends Canvas implements Runnable{
 	public static JFrame frame;
 	private boolean isRunning = true;
 	private Thread thread;
-	private final int WIDTH = 640, HEIGHT = 480, SCALE = 1;
+	private final int WIDTH = 480, HEIGHT = 360, SCALE = 2;
 	private BufferedImage image;
 	
-
+	private Spritesheet sheet;
+	private BufferedImage[] player;
+	private int frames = 0;//Retorno de Frames
+	private int maxFrames = 10;//Velocidade em relação aos frames
+	private int currentAnimation = 0/*Quantidade mínima de sprites para animar*/;
+	private int maxAnimation = 2;//Quantidade total de sprites para animar 
+	
+	private Graphics g;
+	private Graphics2D g2;
+	/***/
+	
+	//Construtor
 	public Game() {
+		/*Player*/
+		sheet = new Spritesheet("/spritesheet.png");
+		player = new BufferedImage[2];
+		player[0] = sheet.getSprite(0, 0, 16, 16);
+		player[1] = sheet.getSprite(16, 0, 16, 16);
+		/***/
+		
 		this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
 		initFrame();
 		image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
 	}
-
+	
+	//Criação da Janela
 	public void initFrame() {
 		frame = new JFrame("New Window");
 		frame.add(this);
@@ -34,7 +55,7 @@ public class Game extends Canvas implements Runnable{
 		frame.setVisible(true);//Dizer que estará visível
 	}
 	
-
+	//Threads
 	public synchronized void start() {
 		thread = new Thread(this);
 		isRunning = true;
@@ -42,7 +63,12 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public synchronized void stop() {
-		
+		isRunning = false;
+		try {
+			thread.join();
+		} catch(InterruptedException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -51,7 +77,14 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public void tick() {
-		
+		frames++;
+		if(frames > maxFrames) {
+			frames = 0;
+			currentAnimation++;
+			if(currentAnimation >= maxAnimation) {
+				currentAnimation = 0;
+			}
+		}
 	}
 	
 	//O que será mostrado em tela
@@ -62,14 +95,29 @@ public class Game extends Canvas implements Runnable{
 			return;
 		}
 		
-		Graphics g = image.getGraphics();//Renderizar imagens na tela
+		g = image.getGraphics();//Renderizar imagens na tela
 		g.setColor(new Color(2, 20, 0));
-		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+		
+		/*Set Font*/
+		g.setFont(new Font("Helvetica", Font.BOLD, 30));
+		g.setColor(Color.white);
+		g.drawString("Hello World", 30, 40);
+		/***/
+		
+		/*Render do jogo*/
+		g2 = (Graphics2D) g; //Transformei em um tipo g e foi feito um cast com o Graphics2D
+		//g2.rotate(Math.toRadians(45), 90+8, 90+8);
+		g2.drawImage(player[currentAnimation], 90, 90, null);
+		/***/
+		
+		g.dispose();//Limpar dados de imagem não usados
 		g = bs.getDrawGraphics();
 		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE,null);
 		bs.show();
 	}
 	
+	//Controle de FPS
 	public void run() {
 		//Variables
 		long lastTime = System.nanoTime();//Usa o tempo atual do computador em nano segundos, bem mais preciso
@@ -87,8 +135,7 @@ public class Game extends Canvas implements Runnable{
 			lastTime = now;
 			
 			if(delta >= 1) {
-				tick();
-				render();
+				tick(); render();
 				frames++;
 				delta--;
 			}
@@ -99,6 +146,8 @@ public class Game extends Canvas implements Runnable{
 				timer += 1000;
 			}
 		}
+		
+		stop(); //Garante que todas as Threads relacionadas ao computador foram terminadas, para garantir performance.
 		
 	}
 	
